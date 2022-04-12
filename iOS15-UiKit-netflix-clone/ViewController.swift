0d3d7b45c96173d1617b1128f9a9dd178350d33a
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-04-12 21:46:58 +0700

    Initial Commit

---
+++ iOS15-UiKit-netflix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS15-UiKit-netflix-clone
+//
+//  Created by TheGIZzz on 12/4/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
