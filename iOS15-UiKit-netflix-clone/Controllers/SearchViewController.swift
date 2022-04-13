7463fc1f8dea5f34fce45b059471199c20fd28be
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-04-13 22:22:44 +0700

    Adding the mainTabBarView with another viewControllers

---
+++ iOS15-UiKit-netflix-clone/Controllers/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  iOS15-UiKit-netflix-clone
+//
+//  Created by TheGIZzz on 13/4/2565 BE.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
