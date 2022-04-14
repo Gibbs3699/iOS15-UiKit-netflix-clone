917783055b465564602420a11922b30fea365864
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-04-15 00:11:00 +0700

    Setting homeViewController TableView done

---
+++ iOS15-UiKit-netflix-clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  iOS15-UiKit-netflix-clone
+//
+//  Created by TheGIZzz on 15/4/2565 BE.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
